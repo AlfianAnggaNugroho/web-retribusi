52bbd4be313d55934327f675407dc288243515a8
Alfian Angga Nugroho <72289023+AlfianAnggaNugroho@users.noreply.github.com>
2024-05-10 14:46:23 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://alfianangganugroho.github.io/web-retribusi/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
