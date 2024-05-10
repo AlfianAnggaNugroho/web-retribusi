a960a4eff4cf4e7b127b126e8b047c068155325c
Alfian Angga Nugroho <72289023+AlfianAnggaNugroho@users.noreply.github.com>
2024-05-10 14:40:00 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/simretribusi/';
+$config['base_url'] = 'https://alfianangganugroho.github.io/web-retribusi//';
 
 /*
 |--------------------------------------------------------------------------
