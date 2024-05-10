f7b603a6f21c63b2a4a3865089dd3b0fc4b9dc4b
Alfian Angga Nugroho <72289023+AlfianAnggaNugroho@users.noreply.github.com>
2024-05-10 14:43:30 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://alfianangganugroho.github.io/web-retribusi//';
+$config['base_url'] = 'https://alfianangganugroho.github.io/web-retribusi/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://alfianangganugroho.github.io/web-retribusi//';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
